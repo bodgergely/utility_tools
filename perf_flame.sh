751bb359b77db7e55e73df5293bfaf950bc480dd
Gergely Bod <bodgergely@hotmail.com>
2019-12-04 19:33:21 +0000

    perf flame graph

--- perf_flame.sh
+++ perf_flame.sh
@@ -1,3 +1,4 @@
+BIN=`which perf`
 PERF_DATA_PATH=/tmp/perf.data
 PERF_SVG_PATH=/tmp/perf.svg
 
@@ -9,6 +10,6 @@ if [ "$#" -gt 1 ]; then
     PERF_SVG_PATH=$2
 fi
 
-sudo perf record -F 99 -a --call-graph dwarf -o $PERF_DATA_PATH -- sleep 10
+sudo $BIN record -F 99 -a --call-graph dwarf -o $PERF_DATA_PATH -- sleep 10
 perf_to_flame.sh $PERF_DATA_PATH $PERF_SVG_PATH
 google-chrome $PERF_SVG_PATH &
--- perf_to_flame.sh
+++ perf_to_flame.sh
@@ -1,3 +1,6 @@
+
+BIN=`which perf`
+
 PERF_DATA_PATH=/tmp/perf.data
 PERF_SVG_PATH=/tmp/perf.svg
 
@@ -9,6 +12,6 @@ if [ "$#" -gt 1 ]; then
     PERF_SVG_PATH=$2
 fi
 
-sudo perf script -i $PERF_DATA_PATH | stackcollapse-perf.pl | flamegraph.pl --width 2000 --height 20 > $PERF_SVG_PATH
-sudo rm $PERF_DATA_PATH
+sudo $BIN script -i $PERF_DATA_PATH | stackcollapse-perf.pl | flamegraph.pl --width 2000 --height 20 > $PERF_SVG_PATH
+#sudo rm $PERF_DATA_PATH
 sudo chmod 666 $PERF_SVG_PATH
